6b24efcf22d0ae098859a8443093e136a4e894c8
lylei <leiliyuan@baidu.com>
2015-11-19 10:32:08 +0800

    localhost

--- conf.cc
+++ conf.cc
@@ -12,6 +12,5 @@ int CONF_sdk_thread_num = 4;
 int CONF_sdk_put_pending = 1;
 
 // server
-std::string CONF_server_addr = "st01-spi-session1.st01.baidu.com";
-//std::string CONF_server_addr = "yq01-tera-dev1.yq01.baidu.com";
+std::string CONF_server_addr = "localhost";
 std::string CONF_server_port = "8221";
